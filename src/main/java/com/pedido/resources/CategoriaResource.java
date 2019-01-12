bb8033f612dec10ea22ffd8517294905edf90b19
Dailan Bueno <daylansantos@gmail.com>
2019-01-12 17:23:11 -0200

    Devolvendo categoria por serviço rest

--- src/main/java/com/pedido/resources/CategoriaResource.java
+++ src/main/java/com/pedido/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.pedido.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedido.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 		
 	@RequestMapping(method=RequestMethod.GET) 	
-	public String listar() {
-		return "Bem vindo ";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 }
