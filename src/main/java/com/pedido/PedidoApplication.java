7f456aecdc70a180171f4acc1b610f7385104a70
Dailan Bueno <daylansantos@gmail.com>
2019-01-13 17:28:45 -0200

    Criando insert automático para categorias

--- src/main/java/com/pedido/PedidoApplication.java
+++ src/main/java/com/pedido/PedidoApplication.java
@@ -1,13 +1,30 @@
 package com.pedido;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pedido.domain.Categoria;
+import com.pedido.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PedidoApplication {
+public class PedidoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRespository.save(Arrays.asList(cat1,cat2));
 	}
 
 }
