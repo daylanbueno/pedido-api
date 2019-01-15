d2b19eab12c0ee0d22ffa86a04ca04f9af93dae8
Dailan Bueno <daylansantos@gmail.com>
2019-01-15 21:16:23 -0200

    Fazendo mapeamento de insert inicias para cidade e estado.

--- src/main/java/com/pedido/PedidoApplication.java
+++ src/main/java/com/pedido/PedidoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pedido.domain.Categoria;
+import com.pedido.domain.Cidade;
+import com.pedido.domain.Estado;
 import com.pedido.domain.Produto;
 import com.pedido.repositories.CategoriaRepository;
+import com.pedido.repositories.CidadeRepository;
+import com.pedido.repositories.EstadoRepository;
 import com.pedido.repositories.ProdutoRespository;
 
 @SpringBootApplication
@@ -21,6 +25,11 @@ public class PedidoApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRespository produtoRepository;
 
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoApplication.class, args);
@@ -41,9 +50,21 @@ public class PedidoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().add(cat2);
 		
+		
+		Estado df = new Estado(null, "Distrito Federal");
+		Estado sp = new Estado(null, "São Paulo");
+		
+		Cidade sobradinho = new Cidade(null,"Sobradinho",df);
+		Cidade aguasclaras = new Cidade(null,"Aguas Claras",df);
+		Cidade campinas  = new Cidade(null, "Campinas",sp);
+		df.getCidades().addAll(Arrays.asList(sobradinho,aguasclaras));
+		sp.getCidades().add(campinas);
+		
+		
 		categoriaRespository.save(Arrays.asList(cat1,cat2));
 		produtoRepository.save(Arrays.asList(p1,p2,p3));
-		
+		estadoRepository.save(Arrays.asList(df,sp));
+		cidadeRepository.save(Arrays.asList(sobradinho,aguasclaras,campinas));
 	}
 
 }
--- src/main/java/com/pedido/services/CategoriaService.java
+++ src/main/java/com/pedido/services/CategoriaService.java
@@ -6,17 +6,18 @@ import org.springframework.stereotype.Service;
 import com.pedido.domain.Categoria;
 import com.pedido.repositories.CategoriaRepository;
 import com.pedido.services.exeption.ObjectNotFoundExeption;;;
+
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Integer id) {
-		Categoria  categoria = categoriaRepository.findOne(id); 
-		if(categoria == null) {
-			throw  new ObjectNotFoundExeption("Objeto não encontrato id:"+id);
+		Categoria categoria = categoriaRepository.findOne(id);
+		if (categoria == null) {
+			throw new ObjectNotFoundExeption("Objeto não encontrato id:" + id);
 		}
-		return  categoria;
+		return categoria;
 	}
 }
